4c9981cf1be91a889a9f7bcbd2df2498a6f75676
Ruben <mandrakelinux07@gmail.com>
2017-07-24 02:55:19 -0300

    Pruebas enviando objetos a servidor

--- PruebaTrabajoEspecial/src/prueba/login/pass/con/hash/Cliente.java
+++ PruebaTrabajoEspecial/src/prueba/login/pass/con/hash/Cliente.java
@@ -1,9 +1,15 @@
 package prueba.login.pass.con.hash;
 
 import java.awt.event.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 import javax.swing.*;
 
+import java.net.*;
 
 public class Cliente {
 
@@ -11,7 +17,25 @@ public class Cliente {
 		// TODO Auto-generated method stub
 		
 		MarcoCliente mimarco=new MarcoCliente();
-		
+		/*
+		mimarco.getContentPane().addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				try {
+					Socket botonPResinado=new Socket("localhost", 9999);
+					DataOutputStream salida=new DataOutputStream(botonPResinado.getOutputStream());
+					salida.writeUTF(e.getKeyChar()+"");
+					salida.close();
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		*/
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
@@ -23,7 +47,7 @@ class MarcoCliente extends JFrame{
 	
 	public MarcoCliente(){
 		
-		setBounds(600,300,280,350);
+		setBounds(600,300,240,350);
 				
 		LaminaMarcoCliente milamina=new LaminaMarcoCliente();
 		
@@ -37,10 +61,22 @@ class MarcoCliente extends JFrame{
 class LaminaMarcoCliente extends JPanel{
 	
 	public LaminaMarcoCliente(){
-	
-		JLabel texto=new JLabel("CLIENTE");
+		
+		this.nick=new JTextField(5);
+		
+		add(nick);
+		
+		JLabel texto=new JLabel("Chat");
 		
 		add(texto);
+		
+		this.ip=new JTextField(8);
+		
+		add(ip);
+		
+		this.campoChat=new JTextArea(12,20);
+		
+		add(campoChat);
 	
 		campo1=new JTextField(20);
 	
@@ -50,18 +86,81 @@ class LaminaMarcoCliente extends JPanel{
 		
 		EnviaTexto miEvento=new EnviaTexto();//Clase interna
 		
+		//EnviaTecla tecla=new EnviaTecla();
+		
+		//campo1.addKeyListener(tecla);
+		
 		miboton.addActionListener(miEvento);//Pongo el boton a la escucha de la instancia de mi evento
 		
 		add(miboton);	
 		
+		
 	}
-	
+	private class EnviaTecla implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+				try{
+						Socket botonPResinado=new Socket("localhost", 9999);
+						DataOutputStream salida=new DataOutputStream(botonPResinado.getOutputStream());
+						salida.writeUTF(e.getKeyChar()+"");
+						salida.close();
+					} catch (UnknownHostException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 	private class EnviaTexto implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println(campo1.getText());
-			
+			//System.out.println(campo1.getText());
+			//String insertado=JOptionPane.showInputDialog("Ingresa la IP DEL SERVIDOR");
+			String insertado=JOptionPane.showInputDialog("Ingresa la IP DEL SERVIDOR","localhost");
+			//System.out.println(insertado);
+			try {
+				
+				Socket miSocket=new Socket(insertado, 9999);
+				
+				//Voy a enviar estos datos a un cliente por medio del Servidor como intermediario 
+				PaqueteEnvio datos=new PaqueteEnvio();
+				datos.setIp(ip.getText());
+				datos.setNick(nick.getText());
+				datos.setMensaje(campo1.getText());
+				
+				ObjectOutputStream paqueteDatos=new ObjectOutputStream(miSocket.getOutputStream());
+				paqueteDatos.writeObject(datos);
+				paqueteDatos.close();
+				//DataOutputStream flujoSalida=new DataOutputStream(miSocket.getOutputStream());
+				//flujoSalida.writeUTF(campo1.getText());
+				//flujoSalida.close();//cierra el FLUJO
+				
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// Puede pasar que la ip este mal dado, que este ocupado, que haya
+				///congestion en la red x el trafico, etc.
+				System.out.println(e1.getMessage());
+				JOptionPane.showMessageDialog(null, "Servidor sin respuesta");
+			}
 		}
 		
 	}
@@ -69,8 +168,39 @@ class LaminaMarcoCliente extends JPanel{
 		
 		
 		
-	private JTextField campo1;
+	private JTextField campo1, nick, ip;
 	
 	private JButton miboton;
 	
+	private JTextArea campoChat;
+	
+	class PaqueteEnvio implements Serializable{
+		
+		private String ip;
+		
+		private String nick;
+		
+		private String mensaje;
+		
+		public String getIp() {
+			return ip;
+		}
+		public void setIp(String ip) {
+			this.ip = ip;
+		}
+		public String getNick() {
+			return nick;
+		}
+		public void setNick(String nick) {
+			this.nick = nick;
+		}
+		public String getMensaje() {
+			return mensaje;
+		}
+		public void setMensaje(String mensaje) {
+			this.mensaje = mensaje;
+		}
+		
+		
+	}
 }
--- PruebaTrabajoEspecial/src/prueba/login/pass/con/hash/Servidor.java
+++ PruebaTrabajoEspecial/src/prueba/login/pass/con/hash/Servidor.java
@@ -4,21 +4,32 @@ package prueba.login.pass.con.hash;
 
 import javax.swing.*;
 
+import prueba.login.pass.con.hash.LaminaMarcoCliente.PaqueteEnvio;
+
 import java.awt.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor  {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// El servidor tiene que hacer 2 tareas
+		//1 recibe en textArea texto
+		//En 2do plano tiene que permanecer a la escucha ctemente y tener el puerto 9999 abierto
 		
 		MarcoServidor mimarco=new MarcoServidor();
 		
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 			
 	}	
 }
 
-class MarcoServidor extends JFrame {
+class MarcoServidor extends JFrame implements Runnable{ //clase que contruye el marco
 	
 	public MarcoServidor(){
 		
@@ -36,7 +47,58 @@ class MarcoServidor extends JFrame {
 		
 		setVisible(true);
 		
+		Thread miHilo=new Thread(this);
+		miHilo.start();
+		
 		}
 	
 	private	JTextArea areatexto;
+
+	@Override
+	public void run() {
+		//El codigo que estará ala escucha
+		//System.out.println("EStoy a la escucha");
+		try {
+			
+				
+				//Aca  no es necesario el while(1){}. No es necesario volver a poner
+				//a la escucha porque esta sentencia YA LA HACE EL HILO
+				ServerSocket servidor=new ServerSocket(9999);
+				//Sí es importante que acepte de nuevo las conexiones
+				
+				String nick, ip, mensaje;
+				PaqueteEnvio paqueteRecibido;
+			while (true) {	
+				Socket miSocket=servidor.accept();//
+				
+				ObjectInputStream paqueteDatos=new ObjectInputStream(miSocket.getInputStream());
+				
+				paqueteRecibido=(PaqueteEnvio) paqueteDatos.readObject();
+				//////////ATENCION AL PAQUETE/////////
+				ip=paqueteRecibido.getIp();
+				nick=paqueteRecibido.getNick();
+				mensaje=paqueteRecibido.getMensaje();
+				
+				areatexto.append(nick+": "+mensaje+" para "+ip+"\n");
+				//DataInputStream flujoEntrada=new DataInputStream(miSocket.getInputStream());//Creo un flujo de entrada
+				
+				//String mensajeTexto=flujoEntrada.readUTF();
+				
+				//areatexto.append(mensajeTexto+"\n");
+				
+			
+				miSocket.close();//CIERRO EL SOCKET DEL CLIENTE QUE LLEGO
+			}
+			/*
+			 Recordar que todo este bloque esta dentro de un HILO EJECUTANDO EN
+			 2DO PLANO. Lo que nos permite manejar la app en 1ER PLANO, por ej,
+			 si la App tuviera menus (U OTRAS COSAS QUE HACER) las podemos trabajar
+			 EN 1ER PLANO xq en 
+			 1ER PLANO esta ejecutando una cosa y en 2DO PLANO OTRA
+			 */
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
